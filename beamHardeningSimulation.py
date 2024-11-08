a871524890a5dad7f73b7f0025acf7236ad1e7fe
Xilin-L <u5794603@anu.edu.au>
2024-11-08 15:23:37 +1100

    packed the simulation to the function runSimulation

--- beamHardeningSimulation.py
+++ beamHardeningSimulation.py
@@ -117,6 +117,7 @@ def projectWithBeamHardeningModel(imgIn, A, n, angles=None, vxSzMm=1.0):
 
 
 def setSpectrum(kvp, filterMaterial='Al', filterThicknessMm=0.5, plot=False):
+    # spectrum filtered and detected
     energyKeV, spectrumIn = xs.generateEmittedSpectrum(kvp, filterMaterial, filterThicknessMm)
     spectrumDet = xs.detectedSpectrum(energyKeV, spectrumIn)
     if plot is True:
@@ -124,80 +125,41 @@ def setSpectrum(kvp, filterMaterial='Al', filterThicknessMm=0.5, plot=False):
     return energyKeV, spectrumDet
 
 
-def setMaterial(materialName):
-    if materialName.lower() == "al" or materialName.lower() == "aluminium":
-        materialWeights = [1.0]
-        materialSymbols = ["Al"]
-        dens = 2.7
-    elif materialName.lower() == "ti" or materialName.lower() == "titanium":
-        materialWeights = [1.0]
-        materialSymbols = ["Ti"]
-        dens = 4.51
-    elif materialName.lower() == "pmma" or materialName.lower() == "acrylic":
-        materialWeights = [0.080541, 0.599846, 0.319613]
-        materialSymbols = ["H", "C", "O"]
-        dens = 1.18
-    elif materialName.lower() == "sio2" or materialName.lower() == "glass":
-        materialWeights, materialSymbols, dens = xip.getMaterialProperties("sandstone")
-    elif materialName.lower() == "caco3" or materialName.lower() == "marble":
-        materialWeights, materialSymbols, dens = xip.getMaterialProperties("carbonate")
-    else:
-        raise Exception(
-            "unknown material name %s. Currently have in dictionary: Aluminium, Titanium, Acrylic, Glass, Marble." % (
-                materialName))
-    return materialWeights, materialSymbols, dens
-
-
 def getMaterialAttenuation(energyKeV, materialWeights, materialSymbols, dens):
     sampleAttPerCm = dens * xs.calcMassAttenuation(energyKeV, materialWeights, materialSymbols)
     return sampleAttPerCm
 
 
-if __name__ == "__main__":
-    Ny = Nx = 128
+def runSimulation(Ny=128, Nx=128, vxSzMm=0.1, sampleDiameterMm=10., kvp=60, filterMaterial='Al',
+                  filterThicknessMm=0.5, materialName='marble', plotIdeal=False,plotBH=True):
+
     imgShape = (Ny, Nx)
-
-    vxSzMm = 0.1
-    sampleDiameterMm = 10.
-
     img = generateCylinder(imgShape, diameterMm=sampleDiameterMm, vxSzMm=vxSzMm)
-    plt.imshow(img, "gray")
-    plt.show()
 
     ### SIMPLE TEST ###
     sinogramIdeal = project(img, vxSzMm=vxSzMm)
-    plt.imshow(sinogramIdeal, "gray")
-    plt.show()
-
     recon = reconstruct(sinogramIdeal)
-    plt.imshow(recon, "gray")
-    plt.show()
+
+    if plotIdeal:
+        plt.imshow(img, "gray") # plot the truth at given resolution
+        plt.show()
+        plt.imshow(sinogramIdeal, "gray") # plot the ideal sinogram (ideal -> defect-free)
+        plt.show()
+        plt.imshow(recon, "gray") # plot the idea reconstruction
+        plt.show()
 
     ### ADD SPECTRUM INFO ###
-
-    # set spectrum
-    energyKeV, spectrum = setSpectrum(kvp=60, filterMaterial='Al', filterThicknessMm=0.5)
-
-    # set material to "sandstone"
-    materialWeights, materialSymbols, dens = setMaterial("marble")
-
-    # get material attenuation
+    energyKeV, spectrum = setSpectrum(kvp=kvp, filterMaterial=filterMaterial, filterThicknessMm=filterThicknessMm)
+    materialWeights, materialSymbols, dens = xip.getMaterialProperties(materialName)
     sampleAttPerCm = getMaterialAttenuation(energyKeV, materialWeights, materialSymbols, dens)
-
-    # estimate beam hardening
     A, n = xip.estimateBeamHardening(spectrum, sampleAttPerCm, sampleDiameterMm, plot=True)
     print("BHC parms [A, n] = [%s, %s], bhcFactor = %s" % (A, n, 1. / n))
 
-    # project with spectrum
     sinogram = projectSingleMaterialWithSpectrum(spectrum, sampleAttPerCm, img, angles=None, vxSzMm=vxSzMm)
-    plt.imshow(sinogram, "gray")
-    plt.show()
 
-    # scale ideal sinogram to match
     meanAttenuation = np.mean(sinogram) / np.mean(sinogramIdeal)
     sinogramIdeal *= meanAttenuation
 
-    # estiamate equivalent sinogram using model
     sinogramBH = projectWithBeamHardeningModel(img, A, n, vxSzMm=vxSzMm)
 
     plt.plot(sinogramIdeal[:, 0], label="monochrom.")
@@ -207,10 +169,17 @@ if __name__ == "__main__":
     plt.show()
 
     recon = reconstruct(sinogram)
-    plt.imshow(recon, "gray")
-    plt.show()
+    if plotBH: # plot the beam-hardened sinogram and reconstruction
+        plt.imshow(sinogram, "gray")
+        plt.show()
+        plt.imshow(recon, "gray")
+        plt.show()
 
     plt.plot(recon[Ny // 2, :])
     plt.show()
+
+if __name__ == "__main__":
+
+    runSimulation(kvp=120, plotIdeal=False,plotBH=False)
 
     exit()
